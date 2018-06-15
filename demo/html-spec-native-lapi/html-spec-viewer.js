a1675e2c7518f43fb0eae25bbe30e7c3dfc7ac26
valdrinkoshi <valdrinkoshi@gmail.com>
2018-06-15 10:44:07 -0700

    seed gh-pages

---
+++ demo/html-spec-native-lapi/html-spec-viewer.js
@@ -0,0 +1,66 @@
+import {VirtualScrollerElement} from 'std:virtual-scroller';
+
+import {HtmlSpec} from '../../components/streaming-spec/HtmlSpec.js';
+import {iterateStream} from '../../components/streaming-spec/iterateStream.js';
+
+class HTMLSpecViewer extends VirtualScrollerElement {
+  connectedCallback() {
+    super.connectedCallback();
+    if (!this._htmlSpec) {
+      const style = document.createElement('style');
+      style.textContent = `
+  :host {
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    right: 0px;
+    bottom: 0px;
+    padding: 8px;
+    height: auto;
+  }`;
+      this.shadowRoot.appendChild(style);
+      if ('rootScroller' in document) {
+        document.rootScroller = this;
+      }
+
+      this._htmlSpec = new HtmlSpec();
+      this._htmlSpec.head.style.display = 'none';
+      this.appendChild(this._htmlSpec.head);
+
+      this.items = [];
+      this.newChild = (item) => item;
+      this.addNextChunk();
+      this.addEventListener(
+          'rangechange', (event) => this.onRangechange(event));
+    }
+  }
+
+  async addNextChunk(chunk = 10) {
+    if (this._adding) {
+      return;
+    }
+    this._adding = true;
+    const stream = this._htmlSpec.advance(this.items[this.items.length - 1]);
+    for await (const el of iterateStream(stream)) {
+      if (/^(style|link|script)$/.test(el.localName)) {
+        this._htmlSpec.head.appendChild(el);
+      } else {
+        this.items.push(el);
+        chunk--;
+      }
+      if (chunk === 0) {
+        this.requestReset();
+        break;
+      }
+    }
+    this._adding = false;
+  }
+
+  onRangechange(range) {
+    if (range.last >= this.items.length - 4) {
+      this.addNextChunk();
+    }
+  }
+}
+
+customElements.define('html-spec-viewer', HTMLSpecViewer);
---
+++ smoke/flat/script.js
@@ -0,0 +1,62 @@
+import Layout from '../../layouts/layout-1d.js';
+import {scroller} from '../../lit-html/lit-scroller.js';
+import {html, render} from '../../components/lit-html/lib/lit-extended.js';
+import {VirtualScroller} from '../../virtual-scroller.js';
+
+const items = new Array(200).fill({name: 'item'});
+const container = document.getElementById('container');
+
+const layout = new Layout({itemSize: {height: 50}});
+// render(
+//     html`${scroller({
+//       items,
+//       template: (item, idx) => html`
+//         <section><div class="title">${idx} - ${item.name}</div></section>
+//       `,
+//       layout,
+//     })}`,
+//     container);
+const pool = [];
+const config = {
+  totalItems: items.length,
+  container,
+  layout,
+  createElement: (idx) => {
+    let section = pool.pop();
+    if (!section) {
+      section = document.createElement('section');
+      section.innerHTML = `<div class="title"></div>`;
+      section._title = section.querySelector('.title');
+      // Update it immediately.
+      config.updateElement(section, idx);
+    }
+    return section;
+  },
+  updateElement: (section, idx) => {
+    const item = items[idx];
+    section.id = `section_${idx}`;
+    section._title.textContent = `${idx} - ${item.name}`;
+  },
+  recycleElement: (section) => {
+    pool.push(section);
+  }
+};
+window.scroller = new VirtualScroller(config);
+
+// document.body.style.minHeight = (innerHeight * 100) + 'px'
+
+// container.style.display = 'none';
+// setTimeout(() => {
+//   container.style.display = '';
+//   scroller.requestReset();
+// }, 1000);
+
+// setInterval(() => {
+//   Array.from(container.children).forEach(section => {
+//     section.appendChild(document.createElement('input'));
+//   })
+// }, 2000);
+
+// setTimeout(() => {
+//   scroller.splice(0, 0, {name: 'new'});
+// }, 1000);
